0a3e0e13bbabea07b093206e5f22fe931953da6d
James Cameron Abreu <coolcam222@gmail.com>
2025-06-13 13:06:17 -0600

    Massive Voldy upgrade

--- HPScreen/Entities/Voldy.cs
+++ HPScreen/Entities/Voldy.cs
@@ -40,6 +40,7 @@ namespace HPScreen.Entities
         public const int ANIMATION_INDEX_MAX = 50;
         public int AnimationIndexTimer { get; set; }
         public Vector2 AnimationOriginPoint { get; set; }
+        public float SpiralPositionIndex { get; set; }
         public Voldy() : base()
         {
             ResetAnimation();
@@ -54,6 +55,7 @@ namespace HPScreen.Entities
             if (HallucinateTimer == 0)
             {
                 IsAnimating = true;
+                SpiralPositionIndex = 0;
                 int ranX = Ran.Current.Next((int)(Graphics.Current.ScreenWidth * 0.3), (int)(Graphics.Current.ScreenWidth * 0.7));
                 int ranY = Ran.Current.Next((int)(Graphics.Current.ScreenHeight * 0.3), (int)(Graphics.Current.ScreenHeight * 0.7));
                 AnimationOriginPoint = new Vector2(ranX, ranY);
@@ -79,41 +81,63 @@ namespace HPScreen.Entities
             if (IsAnimating)
             {
                 Graphics.Current.SpriteB.Begin();
-                for (int i = 0; i < AnimationIndex; i++)
-                {
-                    Vector2 curvoldy = GenerateSpiralPoint(AnimationOriginPoint, i);
-                    float scaleFactor = (float)(Scale * 5 * Math.Pow(AnimationIndex / ANIMATION_INDEX_MAX, 2));
-                    Graphics.Current.SpriteB.Draw(
-                        Graphics.Current.SpritesByName["voldy"], // (Texture2D texture,
-                            curvoldy, // Vector2 position,
-                            null,               // Rectangle? sourceRectangle,
-                            Color.White,        // Color color,
-                            0,      // float rotation,
-                            new Vector2(SpriteWidth / 2, SpriteHeight / 2),       // Vector2 origin,
-                            Vector2.One,        // Vector2 scale,
-                            Flipped ? SpriteEffects.FlipHorizontally : SpriteEffects.None, // SpriteEffects effects,
-                            1);                 // float layerDepth)
-                }
+                DrawVoldy();
+                SpiralPositionIndex += 0.4f;
+
                 Graphics.Current.SpriteB.End();
             }
         }
         protected void ResetAnimation()
         {
-            HallucinateTimer = Ran.Current.Next(4500, 21000);
-            //HallucinateTimer = Ran.Current.Next(85, 310);
+            //HallucinateTimer = Ran.Current.Next(4500, 21000);
+            HallucinateTimer = Ran.Current.Next(85, 310);
             AnimationIndex = 0;
             AnimationIndexTimer = 0;
         }
         protected Vector2 GenerateSpiralPoint(Vector2 originPosition, int index)
         {
-            float angle = index * Ran.Current.Next(35f, 45f); // Adjust the angle increment for your desired spacing
-            float radius = (float)Math.Sqrt(index) * 160.0f; // Adjust the growth factor for your desired size
+            float angle = index * 15;
+            float radius = (float)Math.Sqrt(index) * 35.0f; // Adjust the growth factor for your desired size
             double radians = angle * Math.PI / 180.0;
 
             float x = originPosition.X + (float)(radius * Math.Cos(radians));
             float y = originPosition.Y + (float)(radius * Math.Sin(radians));
 
             return new Vector2(x, y);
+        }
+        protected void DrawVoldy()
+        {
+            int fadeFrames = 10;
+            int actualIndex = (int)SpiralPositionIndex;
+            int currentIndex = actualIndex - fadeFrames;
+            int smallest = actualIndex - fadeFrames;
+            while (currentIndex < actualIndex)
+            {
+                if (currentIndex < 0)
+                {
+                    currentIndex++;
+                    continue;
+                }
+
+                float indexTransparency = 1 - ((float)actualIndex - currentIndex) / fadeFrames;
+                DrawVoldyAtIndex(currentIndex, Color.White * indexTransparency);
+                currentIndex++;
+            }
+        }
+        protected void DrawVoldyAtIndex(int index, Color color)
+        {
+            Vector2 curvoldy = GenerateSpiralPoint(AnimationOriginPoint, index);
+            //float scaleFactor = (float)(Scale * 5 * Math.Pow(AnimationIndex / ANIMATION_INDEX_MAX, 2));
+            Graphics.Current.SpriteB.Draw(
+                Graphics.Current.SpritesByName["voldy"], // (Texture2D texture,
+                    curvoldy, // Vector2 position,
+                    null,               // Rectangle? sourceRectangle,
+                    color,        // Color color,
+                    0,      // float rotation,
+                    new Vector2(SpriteWidth / 2, SpriteHeight / 2),       // Vector2 origin,
+                    Vector2.One,        // Vector2 scale,
+                    Flipped ? SpriteEffects.FlipHorizontally : SpriteEffects.None, // SpriteEffects effects,
+                    1);                 // float layerDepth)
         }
     }
 }
